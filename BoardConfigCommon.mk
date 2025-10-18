5128501112451e38162052be0a3183599b54cc01
Alec Chan <alecchangod@gmail.com>
2025-10-18 18:28:11 +0800

    sm7250-common: move defconfig into vendor/
    Change-Id: Icfbfefd4bf3ea07390c24bd4b370fec8c9e0c594

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,7 +109,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048 loop.max_part=7
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" LLVM_IAS=1
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := milito_defconfig vendor/debugfs.config
+TARGET_KERNEL_CONFIG := vendor/milito_defconfig vendor/debugfs.config
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_LLVM_BINUTILS := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7250
